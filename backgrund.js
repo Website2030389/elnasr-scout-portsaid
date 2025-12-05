fa8365af1985b27c2199a420c8474d74849b9c51
Abanob Ashraf <bboba7108@gmail.com>
2025-12-05 06:57:52 -0800

    Update backgrund.js

--- backgrund.js
+++ backgrund.js
@@ -1,15 +1,15 @@
-// const vids = ['v1', 'v2', 'v3'];
-// let i = 0;
-// function playNext() {
-//   vids.forEach(id => document.getElementById(id).style.opacity = 0);
-//   const v = document.getElementById(vids[i]);
-//   v.style.opacity = 1;
-//   v.play();
-//   i = (i + 1) % vids.length;
-//   v.onended = playNext;
-// }
-// playNext();
-// 
+const vids = ['v1', 'v2', 'v3'];
+let i = 0;
+function playNext() {
+  vids.forEach(id => document.getElementById(id).style.opacity = 0);
+  const v = document.getElementById(vids[i]);
+  v.style.opacity = 1;
+  v.play();
+  i = (i + 1) % vids.length;
+  v.onended = playNext;
+}
+playNext();
+
 // ******************marquee***************** //
 document.querySelectorAll(".marquee").forEach((marquee) => {
   const root = document.documentElement;
